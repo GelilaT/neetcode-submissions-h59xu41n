879e16523843cd696d1f73ac42fb82c4683dbcac
Gelila Tefera <101528976+GelilaT@users.noreply.github.com>
2026-05-18 20:10:34 +0300

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.py
@@ -0,0 +1,43 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        
+        parent = {i:i for i in range(n)}
+        graph = defaultdict(list)
+        rank = [1] * n
+        def find(x):
+            if x != parent[x]:
+                parent[x] = find(parent[x])
+            
+            return parent[x]
+
+        def union(x, y):
+            parentX = find(x)
+            parentY = find(y)
+
+            if parentX == parentY:
+                return
+
+            if rank[parentX] > rank[parentY]:
+                parent[parentY] = parentX
+                rank[parentX] += rank[parentY]
+
+            else:
+                parent[parentX] = parentY
+                rank[parentY] += rank[parentX]
+
+
+        for start, dest in edges:
+            union(start, dest)
+
+        count = 0
+        seen = set()
+        # print(parent)
+        for i in range(n):
+            rep = find(i)
+            if rep not in seen:
+                seen.add(rep)
+                count += 1
+        
+        return count
+
+
